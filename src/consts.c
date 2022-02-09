da8f542e3181fd5d1dc9503b3c1b564282cfbff7
vms <michail.vms@gmail.com>
2022-02-09 18:29:01 +0300

    move it to bls12_377

--- src/consts.c
+++ src/consts.c
@@ -8,11 +8,11 @@
 
 /* z = -0xd201000000010000 */
 const vec384 BLS12_381_P = {    /* (z-1)^2 * (z^4 - z^2 + 1)/3 + z */
-    TO_LIMB_T(0xb9feffffffffaaab), TO_LIMB_T(0x1eabfffeb153ffff),
-    TO_LIMB_T(0x6730d2a0f6b0f624), TO_LIMB_T(0x64774b84f38512bf),
-    TO_LIMB_T(0x4b1ba7b6434bacd7), TO_LIMB_T(0x1a0111ea397fe69a)
+    TO_LIMB_T(0x8508c00000000001), TO_LIMB_T(0x170b5d4430000000),
+    TO_LIMB_T(0x1ef3622fba094800), TO_LIMB_T(0x1a22d9f300f5138f),
+    TO_LIMB_T(0xc63b05c06ca1493b), TO_LIMB_T(0x1ae3a4617c510ea)
 };
-const limb_t BLS12_381_p0 = (limb_t)0x89f3fffcfffcfffd;  /* -1/P */
+const limb_t BLS12_381_p0 = (limb_t)0x8508bfffffffffff;  /* -1/P */
 
 const radix384 BLS12_381_Rx = { /* (1<<384)%P, "radix", one-in-Montgomery */
   { { ONE_MONT_P },
@@ -20,17 +20,17 @@ const radix384 BLS12_381_Rx = { /* (1<<384)%P, "radix", one-in-Montgomery */
 };
 
 const vec384 BLS12_381_RR = {   /* (1<<768)%P, "radix"^2, to-Montgomery */
-    TO_LIMB_T(0xf4df1f341c341746), TO_LIMB_T(0x0a76e6a609d104f1),
-    TO_LIMB_T(0x8de5476c4c95b6d5), TO_LIMB_T(0x67eb88a9939d83c0),
-    TO_LIMB_T(0x9a793e85b519952d), TO_LIMB_T(0x11988fe592cae3aa)
+    TO_LIMB_T(0xb786686c9400cd22), TO_LIMB_T(0x329fcaab00431b1),
+    TO_LIMB_T(0x22a5f11162d6b46d), TO_LIMB_T(0xbfdf7d03827dc3ac),
+    TO_LIMB_T(0x837e92f041790bf9), TO_LIMB_T(0x6dfccb1e914b88)
 };
 
 const vec256 BLS12_381_r = {    /* z^4 - z^2 + 1, group order */
-    TO_LIMB_T(0xffffffff00000001), TO_LIMB_T(0x53bda402fffe5bfe),
-    TO_LIMB_T(0x3339d80809a1d805), TO_LIMB_T(0x73eda753299d7d48)
+    TO_LIMB_T(9015221291577245683ULL), TO_LIMB_T(8239323489949974514ULL),
+    TO_LIMB_T(1646089257421115374ULL), TO_LIMB_T(958099254763297437ULL)
 };
 
 const vec256 BLS12_381_rRR = {  /* (1<<512)%r, "radix"^2, to-Montgomery */
-    TO_LIMB_T(0xc999e990f3f29c6d), TO_LIMB_T(0x2b6cedcb87925c23),
-    TO_LIMB_T(0x05d314967254398f), TO_LIMB_T(0x0748d9d99f59ff11)
+    TO_LIMB_T(2726216793283724667ULL), TO_LIMB_T(14712177743343147295ULL),
+    TO_LIMB_T(12091039717619697043ULL), TO_LIMB_T(81024008013859129ULL)
 };
--- src/consts.h
+++ src/consts.h
@@ -9,22 +9,22 @@
 
 extern const vec384 BLS12_381_P;
 extern const limb_t BLS12_381_p0;
-static const limb_t p0 = (limb_t)0x89f3fffcfffcfffd;  /* -1/P */
+static const limb_t p0 = (limb_t)0x8508bfffffffffff;  /* -1/P */
 typedef union { vec384 p12[12]; vec384x p2; vec384 p; } radix384;
 extern const radix384 BLS12_381_Rx; /* (1<<384)%P, "radix", one-in-Montgomery */
 extern const vec384 BLS12_381_RR;   /* (1<<768)%P, "radix"^2, to-Montgomery   */
 
-#define ONE_MONT_P TO_LIMB_T(0x760900000002fffd), \
-                   TO_LIMB_T(0xebf4000bc40c0002), \
-                   TO_LIMB_T(0x5f48985753c758ba), \
-                   TO_LIMB_T(0x77ce585370525745), \
-                   TO_LIMB_T(0x5c071a97a256ec6d), \
-                   TO_LIMB_T(0x15f65ec3fa80e493)
+#define ONE_MONT_P TO_LIMB_T(0x02cdffffffffff68), \
+                   TO_LIMB_T(0x51409f837fffffb1), \
+                   TO_LIMB_T(0x9f7db3a98a7d3ff2), \
+                   TO_LIMB_T(0x7b4e97b76e7c6305), \
+                   TO_LIMB_T(0x4cf495bf803c84e8), \
+                   TO_LIMB_T(0x008d6661e2fdf49a)
 
 #define ZERO_384 (BLS12_381_Rx.p2[1])
 
 extern const vec256 BLS12_381_r;    /* order */
-static const limb_t r0 = (limb_t)0xfffffffeffffffff;  /* -1/r */
+static const limb_t r0 = (limb_t)0xa117fffffffffff;  /* -1/r */
 extern const vec256 BLS12_381_rRR;  /* (1<<512)%r, "radix"^2, to-Montgomery   */
 
 #endif
